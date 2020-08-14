bf7d51864143bdcd78e7924cb38d632feee2dbad
AJ Gambino <aj.gambino7@gmail.com>
2020-08-14 13:04:21 -0400

    MVP

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -2,6 +2,32 @@ import React, { useEffect, useState } from 'react';
 import './App.css';
 import Character from './components/Character'
 import axios from 'axios'
+import styled from 'styled-components'
+
+const WrapperDiv =styled.div`
+font-family: sans-serif;
+color: #EE82EE;
+font-weight: bold;
+text-shadow: 2px 1px black;
+font-size:1.5rem;
+list-style: none;
+background: url("https://i.pinimg.com/originals/d9/4f/39/d94f397d1fbebd2212a9b83415d7e550.jpg") no-repeat center/cover;
+`
+
+const Header = styled.h1`
+color:white;
+font-size: 3rem;
+text-shadow: 2px 1px black;
+`
+
+const Flexbox = styled.div`
+display:flex;
+flex-wrap:wrap;
+justify-content: space-around;
+padding-bottom: 14rem;
+width: 80%;
+margin: 0 auto;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -11,7 +37,7 @@ const [characters, setCharacters] = useState([])
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
   useEffect(()=>{
-    axios.get("https://rickandmortyapi.com/api//character/[1,2,244,265]").then(res=>{
+    axios.get("https://rickandmortyapi.com/api//character/[1,2,49,185, 192, 218, 213, 244,265, 312]").then(res=>{
     console.log(res.data)  
     setCharacters(res.data)
     }).catch(err=>{
@@ -21,14 +47,15 @@ const [characters, setCharacters] = useState([])
 
 
   return (
+    <WrapperDiv>
     <div className="App">
-      <h1 className="Header">Rick and Morty Characters</h1>
-      {characters.map(char=>{
+   <Header className="Header">Rick and Morty Characters </Header>
+  <Flexbox>    {characters.map(char=>{
   return  <Character name={char.name} status={char.status} species={char.species} image={char.image}/>
 })
-}
-      
+}</Flexbox>
     </div>
+    </WrapperDiv>
   );
 }
 
--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,11 +1,17 @@
 // Write your Character component here
 import React from 'react'
-//import styled from 'styled-components'
+import styled from 'styled-components'
 
 export default function Character(props){
     const  {name, status, species, image} = props
 
-
+   const Nameh3 = styled.h3`
+   color: white;
+   text-shadow: 2px 1px green;
+   `
+   const ListStyles = styled.li`
+   margin-bottom: 2rem;
+   `
 
   //  if (!props.image) return <h3>Loading...</h3>;
     return (
@@ -13,9 +19,10 @@ export default function Character(props){
 <div>
 
 <img src={image} alt="character"/>
-<h3>{name}</h3>
-<li> {status}</li>
-<li> {species}</li>
+
+<Nameh3>{name}</Nameh3>
+<li>Status: {status}</li>
+<ListStyles>Species: {species}</ListStyles>
 
 </div>
     )
