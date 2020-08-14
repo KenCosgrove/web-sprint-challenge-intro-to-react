ca67e4a24a9d548f7dc4577d4df04c84be1eea34
AJ Gambino <aj.gambino7@gmail.com>
2020-08-14 11:47:50 -0400

    component

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+import React from 'react'
+import styled from 'styled-components'
+
+export default function Character(props){
+    const {name, status, species, image} = props
+    if (!props.image) return <h3>Loading...</h3>;
+    return (
+        
+<div>
+<img src={image} alt="character"/>
+<h3>{name}</h3>
+<li> {status}</li>
+<li> {species}</li>
+
+</div>
+    )
+}
